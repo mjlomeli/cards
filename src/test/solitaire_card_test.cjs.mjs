0af5a084e04d2f113a66abe8eb328bf071e4e32f
mjlomeli <mjlomeli@uci.edu>
2021-11-02 12:24:18 -0700

    added an example of enabling and disabling

--- src/test/solitaire_card_test.cjs.mjs
+++ src/test/solitaire_card_test.cjs.mjs
@@ -10,7 +10,48 @@ var main = function(){
         card.enableDragOnMouseClickHold()
         card.enableFlippingOnClick();
         div.appendChild(card.cardElement);
+
+        let div2 = document.createElement('div');
+        let card2 = new SolitaireCard('hearts', 'queen');
+        await card2.build();
+        card2.enableDragOnMouseClickHold()
+        card2.enableFlippingOnClick();
+        div2.appendChild(card2.cardElement);
         document.body.append(div)
+        document.body.append(div2)
+
+        let one_enabled = true;
+        let button_one = document.createElement('input');
+        button_one.setAttribute("type", "button");
+        button_one.setAttribute("value", "halt card 1");
+        button_one.addEventListener("click", ()=>{
+            if (one_enabled) {
+                card.disableDragOnMouseClickHold();
+                card.disableFlippingOnClick();
+            } else {
+                card.enableDragOnMouseClickHold();
+                card.enableFlippingOnClick();
+            }
+            one_enabled = !one_enabled;
+        });
+
+        let two_enabled = true;
+        let button_two = document.createElement('input');
+        button_two.setAttribute("type", "button");
+        button_two.setAttribute("value", "halt card 2");
+        button_two.addEventListener("click", ()=>{
+            if (two_enabled) {
+                card2.disableDragOnMouseClickHold();
+                card2.disableFlippingOnClick();
+            } else {
+                card2.enableDragOnMouseClickHold();
+                card2.enableFlippingOnClick();
+            }
+            two_enabled = !two_enabled;
+        });
+
+        document.body.append(button_one);
+        document.body.append(button_two);
     });
 }
 
